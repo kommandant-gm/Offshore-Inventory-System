6015f15a9b2f28bcca26c61dcfb27b62a6f68130
kommandant-gm <49630340+kommandant-gm@users.noreply.github.com>
2026-07-08 16:21:43 +0800

    update databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $defaultPassword = Hash::make('Dayang@123');
         $adminPermissions = AccessMatrix::permissionsForRole('admin');
 
         User::query()
             ->where('email', 'admin@admin.com')
+            ->delete();
+
+        User::query()
+            ->where('username', '!=', 'codex')
             ->delete();
 
         User::updateOrCreate(
@@ -35,25 +38,5 @@ class DatabaseSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
-
-        collect([
-            ['name' => 'Marie Sim', 'username' => 'marie.sim', 'email' => 'mariesim@desb.net'],
-            ['name' => 'Patrick Leong', 'username' => 'patrick.leong', 'email' => 'patrickleong@desb.net'],
-            ['name' => 'Duyan Ak Jemat', 'username' => 'duyan', 'email' => 'duyan@desb.net'],
-            ['name' => 'Leekp', 'username' => 'leekp', 'email' => 'leekp@desb.net'],
-            ['name' => 'Christopher Ung', 'username' => 'ung.christopher', 'email' => 'ung.christopher@yahoo.com'],
-            ['name' => 'Alex Leong', 'username' => 'alex.leong', 'email' => 'alexleong@desb.net'],
-        ])->each(function (array $user) use ($defaultPassword): void {
-            User::updateOrCreate(
-                ['email' => $user['email']],
-                [
-                    'name' => $user['name'],
-                    'username' => $user['username'],
-                    'role' => 'viewer',
-                    'password' => $defaultPassword,
-                    'email_verified_at' => now(),
-                ]
-            );
-        });
     }
 }
