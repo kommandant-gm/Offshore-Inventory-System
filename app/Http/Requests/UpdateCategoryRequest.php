c71815ce3b7f4341050195abd7fb62b421ca2528
kommandant-gm <49630340+kommandant-gm@users.noreply.github.com>
2026-07-27 16:17:47 +0800

    update

--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use App\Enums\CategoryType;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
 class UpdateCategoryRequest extends FormRequest
 {
     protected function prepareForValidation(): void
